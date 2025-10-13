40b91cff2943084523016c7751e7a7cfd530a80d
Pavel Bezukladnikov <bezukladnikov.pavel.be@gmail.com>
2025-10-13 14:15:43 +0200

    feat(database): create MM_SpotValidation

---
+++ Coordinates_DataBase/Tables/MM_SpotValidation.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[MM_SpotValidation]
+(
+	[IdSpot] UNIQUEIDENTIFIER NOT NULL, 
+	[IdValidator] UNIQUEIDENTIFIER NOT NULL,
+	[IsValid] BIT NOT NULL DEFAULT 1,
+	[ValidatedAt] DATETIME2 NOT NULL DEFAULT GETDATE(),
+	[UpdatedAt] DATETIME2 NULL,
+	CONSTRAINT [PK_SpotValidation] PRIMARY KEY ([IdSpot], [IdValidator])
+)
