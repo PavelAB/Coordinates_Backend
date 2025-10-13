2fc931a58a6e650a4c6027374ea059fa34f0457b
Pavel Bezukladnikov <bezukladnikov.pavel.be@gmail.com>
2025-10-13 14:32:45 +0200

    fix(database): fix foreign key constraint in MM_SpotValidation

--- Coordinates_DataBase/Tables/MM_SpotValidation.sql
+++ Coordinates_DataBase/Tables/MM_SpotValidation.sql
@@ -5,5 +5,7 @@
 	[IsValid] BIT NOT NULL DEFAULT 1,
 	[ValidatedAt] DATETIME2 NOT NULL DEFAULT GETDATE(),
 	[UpdatedAt] DATETIME2 NULL,
-	CONSTRAINT [PK_SpotValidation] PRIMARY KEY ([IdSpot], [IdValidator])
+	CONSTRAINT [PK_SpotValidation] PRIMARY KEY ([IdSpot], [IdValidator]),
+	CONSTRAINT [FK_SpotValidation_Spot] FOREIGN KEY ([IdSpot]) REFERENCES [Spot]([IdSpot]), 
+	CONSTRAINT [FK_SpotValidation_User] FOREIGN KEY ([IdValidator]) REFERENCES [User]([IdUser]) 
 )
