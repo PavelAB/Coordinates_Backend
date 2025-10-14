7fa40c0bff0d95d3c7d54b2b03842c069b128a65
Pavel Bezukladnikov <bezukladnikov.pavel.be@gmail.com>
2025-10-14 16:45:16 +0200

    feat(cqs): add base service to CQS_Domain

--- Coordinates_CQS_Domain/Services/AuthService.cs
+++ Coordinates_CQS_Domain/Services/AuthService.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Coordinates_CQS_Domain.Services
 {
-    public class AuthService
+    public class AuthService: BaseService
     {
+
+        private readonly DbConnection _dbConnection;
+
+        public AuthService(IConfiguration configuration) : base(configuration, "localDb")
+        {
+
+        }
+
+
     }
 }
---
+++ Coordinates_CQS_Domain/Services/BaseService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coordinates_CQS_Domain.Services
+{
+    public class BaseService
+    {
+        protected readonly string _connectonString;
+        public BaseService(IConfiguration configuration, string connectionString)
+        {
+            _connectonString = configuration.GetConnectionString(connectionString);
+        }
+    }
+}
