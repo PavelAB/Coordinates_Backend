5f5c03b74a5bc395ecd48489579754b294475186
Pavel Bezukladnikov <bezukladnikov.pavel.be@gmail.com>
2025-10-15 13:56:32 +0200

    feat(cqs): initialize JWT for token generation

---
+++ Coordiantes_Tools/Tools/JwtOptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coordiantes_Tools.Tools
+{
+    public class JwtOptions
+    {
+
+        public string Issuer { get; }
+        public string Audience { get; }
+        public string SecurityKey { get; }
+
+        public JwtOptions(string issuer, string audience, string securityKey)
+        {
+            Issuer = issuer;
+            Audience = audience;
+            SecurityKey = securityKey;
+        }
+
+    }
+}
