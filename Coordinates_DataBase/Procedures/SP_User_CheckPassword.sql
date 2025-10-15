ba74dfbcfb84d627264447f52995a80212c4babb
Pavel Bezukladnikov <bezukladnikov.pavel.be@gmail.com>
2025-10-15 16:28:36 +0200

    feat(database): add stored procedure to check user password

---
+++ Coordinates_DataBase/Procedures/SP_User_CheckPassword.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[SP_User_CheckPassword]
+	@login NVARCHAR(100),
+	@password NVARCHAR(100)
+AS
+BEGIN
+	SELECT [IdUser], [FirstName], [LastName], [NickName], [Login], [Email], [Avatar], [CreatedAt], [UpdatedAt] 
+	FROM [User] 
+	WHERE 
+		[login] = @login AND
+		[Password] = [dbo].[SF_HashPassword](@password, [Salt])		
+END
+GO
