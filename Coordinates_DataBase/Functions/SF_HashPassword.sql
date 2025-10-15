707349b7eabb8c9f8e1c65ef700d52dd7de7e1d6
Pavel Bezukladnikov <bezukladnikov.pavel.be@gmail.com>
2025-10-15 14:53:20 +0200

    feat(database): add stored procedure and function to create a user

---
+++ Coordinates_DataBase/Functions/SF_HashPassword.sql
@@ -0,0 +1,13 @@
+﻿CREATE FUNCTION [dbo].[SF_HashPassword]
+(
+	@userPassword NVARCHAR(100),
+	@salt UNIQUEIDENTIFIER
+)
+RETURNS VARBINARY(32)
+AS
+BEGIN
+	DECLARE @saltedPassword NVARCHAR(200)
+	SET @saltedPassword = CONCAT(@userPassword, @salt)
+
+	RETURN HASHBYTES('SHA2_256', @saltedPassword)
+END
