51d6f5300945d99b3c62563227ca5e63e827c69d
Pavel Bezukladnikov <bezukladnikov.pavel.be@gmail.com>
2025-10-16 12:17:02 +0200

    feat(database): create stored procedure to add new spot

---
+++ Coordinates_DataBase/Data/TestProcedures.sql
@@ -0,0 +1,18 @@
+﻿/*
+Modèle de script de post-déploiement							
+--------------------------------------------------------------------------------------
+ Ce fichier contient des instructions SQL qui seront ajoutées au script de compilation.		
+ Utilisez la syntaxe SQLCMD pour inclure un fichier dans le script de post-déploiement.			
+ Exemple :      :r .\monfichier.sql								
+ Utilisez la syntaxe SQLCMD pour référencer une variable dans le script de post-déploiement.		
+ Exemple :      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+--EXEC dbo.SP_Spot_Create
+--	@IdUser = '5A381BFB-2EDA-4AE2-A896-FF47B7A163E2',
+--	@IdEntityType = '1E911C43-CC6E-4C64-8214-59C10F3D85FB',
+--	@IdSurfaceType = '82319C4A-4F20-4E8A-828A-807DB23DDFE0',
+--    @Latitude = 89.56,
+--	@Longitude = 90.56,
+--	@Elevation = 899.56
---
+++ Coordinates_DataBase/Procedures/SP_Spot_Create.sql
@@ -0,0 +1,27 @@
+﻿CREATE PROCEDURE [dbo].[SP_Spot_Create]
+	@Latitude DECIMAL(9,6),
+	@Longitude DECIMAL(9,6),
+	@Elevation DECIMAL(8,2),
+	@IdUser UNIQUEIDENTIFIER = NULL,
+	@IdEntityType UNIQUEIDENTIFIER = NULL,
+	@IdSurfaceType UNIQUEIDENTIFIER = NULL
+AS
+BEGIN
+	IF @IdUser IS NULL
+		RAISERROR('The UserId for CreatedBy cannot be null.', 16, 1);
+	IF @IdEntityType IS NULL
+		RAISERROR('The IdEntityType for EntityType cannot be null.', 16, 1);
+	IF @IdSurfaceType IS NULL
+		RAISERROR('The IdSurfaceType for SurfaceType cannot be null.', 16, 1);
+
+	INSERT INTO [Spot] (Latitude, Longitude, Elevation, IdEntityType, IdSurfaceType, CreatedBy) VALUES 
+	(
+		@Latitude,
+		@Longitude,
+		@Elevation,
+		@IdEntityType,
+		@IdSurfaceType,
+		@IdUser
+	)
+END
+GO
