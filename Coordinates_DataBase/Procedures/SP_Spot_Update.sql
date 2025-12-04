71be6aece1aa7813c877160bc6d6b4fcd0dcd30f
Pavel Bezukladnikov <bezukladnikov.pavel.be@gmail.com>
2025-12-04 09:52:28 +0100

    feat(db): add stored procedures for delete and update spot

---
+++ Coordinates_DataBase/Procedures/SP_Spot_Update.sql
@@ -0,0 +1,39 @@
+﻿CREATE PROCEDURE [dbo].[SP_Spot_Update]
+	@IdSpot UNIQUEIDENTIFIER,
+	@Latitude DECIMAL(9,6),
+	@Longitude DECIMAL(9,6),
+	@Elevation DECIMAL(8,2),
+	@Name NVARCHAR(50),
+	@IsPrivate BIT = 0,
+	@CreatedBy UNIQUEIDENTIFIER,
+	@UpdatedBy UNIQUEIDENTIFIER
+AS
+BEGIN
+	
+	BEGIN TRANSACTION
+		BEGIN TRY
+
+			IF NOT EXISTS (SELECT 1 FROM [Spot] WHERE IdSpot = @IdSpot)
+                RAISERROR('Invalid Spot ID.', 16, 1);
+
+			UPDATE Spot
+			SET 
+				Latitude = @Latitude,
+				Longitude = @Longitude,
+				Elevation = @Elevation,
+				[Name] = @Name,
+				IsPrivate = @IsPrivate,
+				CreatedBy = @CreatedBy,
+				UpdatedBy = @UpdatedBy,
+				UpdatedAt = GETDATE()
+			WHERE IdSpot = @IdSpot
+
+
+		COMMIT
+	END TRY
+	BEGIN CATCH
+		ROLLBACK;
+
+		THROW;
+	END CATCH
+END
---
+++ Coordinates_DataBase/Procedures/Spot/SP_Spot_Delete.sql
@@ -0,0 +1,30 @@
+﻿CREATE PROCEDURE [dbo].[SP_Spot_Delete]
+	@IdSpot UNIQUEIDENTIFIER,
+	@DeletedBy UNIQUEIDENTIFIER
+AS
+BEGIN
+	BEGIN TRANSACTION
+		BEGIN TRY
+
+			IF NOT EXISTS (SELECT 1 FROM [Spot] WHERE IdSpot = @IdSpot)
+                RAISERROR('Invalid Spot ID.', 16, 1);
+				
+			IF NOT EXISTS (SELECT 1 FROM [User] WHERE IdUser = @DeletedBy)
+                RAISERROR('Invalid DeletedBy ID.', 16, 1);
+
+			UPDATE Spot
+			SET
+				IsDeleted = 1,
+				DeletedBy = @DeletedBy,
+				DeletedAt = GETDATE()
+			WHERE IdSpot = @IdSpot
+
+
+			COMMIT
+		END TRY
+	BEGIN CATCH
+		ROLLBACK;
+
+		THROW;
+	END CATCH
+END
